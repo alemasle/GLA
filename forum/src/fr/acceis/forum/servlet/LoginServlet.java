c992374dad7ae7c486e81a2bcfef4e52f8c8a610
alemasle <alexislm35@outlook.fr>
2018-10-02 16:54:45 +0200

    all added

---
+++ forum/src/fr/acceis/forum/servlet/LoginServlet.java
@@ -0,0 +1,33 @@
+package fr.acceis.forum.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String user = req.getParameter("username");
+		String pass = req.getParameter("password");
+		String admin_user = "admin";
+		String pass_admin = "admin";
+		
+		if (admin_user.equals(user) && pass_admin.equals(pass)) {
+			req.getRequestDispatcher("/WEB-INF/jsp/threads.jsp").forward(req, resp);
+		}
+		
+		System.out.println("Failed for user = " + user + "\npass = " + pass);
+		
+		doGet(req, resp);
+	}
+
+}
